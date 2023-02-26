77b24169e60453ca8d1c29195f7cfb967c769741
Fabisch Enrique <drenriquenicol@gmail.com>
2023-02-26 10:27:44 +0300

    LV

--- lib/preuba_auth_web/controllers/user_auth.ex
+++ lib/preuba_auth_web/controllers/user_auth.ex
@@ -161,7 +161,7 @@ defmodule PreubaAuthWeb.UserAuth do
   end
 
   @doc """
-  FOR RPOUTES THAT REQUIRE THE USER TO BE AUTHENTICATED, PASS THROUGH =>
+  FOR ROUTES THAT REQUIRE THE USER TO BE AUTHENTICATED, PASS THROUGH =>
   """
 
   def require_authenticated_user(conn, _opts) do
@@ -171,7 +171,7 @@ defmodule PreubaAuthWeb.UserAuth do
       conn
       |> put_flash(:error, "You must log in to access this page")
       |> maybe_store_return_to()
-      |> redirect(to: Routes.user_session_path(conn, :new))
+      |> redirect(to: Routes.user_login_path(conn, :new))
       |> halt()
     end
   end
--- lib/preuba_auth_web/live/user_reset_password_live/user_reset_password.ex
+++ lib/preuba_auth_web/live/user_reset_password_live/user_reset_password.ex
@@ -1,35 +1,80 @@
-defmodule PreubaAuthWeb.UserResetPasswordLive.UserResetPassword do
+defmodule PruebaAuthWeb.UserResetPasswordLive.UserResetPassword do
   use PreubaAuthWeb, :live_view
 
-  alias PreubaAuth.Accounts
+  alias PruebaAuth.Accounts
 
-  on_mount {PreubaAuthWeb.UserAuth, :get_user_by_reset_password_token}
+  on_mount {PruebaAuthWeb.UserAuth, :get_user_by_reset_password_token}
+
+  def render(%{live_action: :new} = assigns) do
+    ~H"""
+
+    <h1>Forgot your password?</h1>
+
+    <.form let={f} for={:user} phx-submit={"send_email"}>
+
+      <%= label f, :email %>
+      <%= email_input f, :email, required: true %>
+
+      <div>
+        <%= submit "Send instructions to reset password" %>
+      </div>
+
+    </.form>
+
+    """
+  end
+
+  def render(%{live_action: :edit} = assigns) do
+    ~H"""
+    <h1>Reset password</h1>
+
+    <.form let={f} for={@changeset} phx-submit={"reset_password" @token}>
+
+      <%= if @changeset.action do %>
+
+        <div class="alert alert-danger">
+          <p>Oops, something went wrong! Please check the errors below.</p>
+        </div>
+
+      <% end %>
+      <%= label f, :password, "New password" %>
+      <%= password_input f, :password, required: true %>
+      <%= error_tag f, :password %>
+      <%= label f, :password_confirmation, "Confirm new password" %>
+      <%= password_input f, :password_confirmation, required: true %>
+      <%= error_tag f, :password_confirmation %>
+
+      <div>
+        <%= submit "Reset password" %>
+      </div>
+
+    </.form>
+    """
+  end
 
   def mount(_params, _session, socket) do
     if socket.assigns.live_action in [:edit, :update] do
       changeset = Accounts.change_user_password(socket.assigns.user)
-
-      {:ok,
-       socket
-       |> assign(:changeset, changeset)}
+      assign(socket, :changeset, changeset)
+      {:ok, assign(socket, :changeset, changeset)}
     else
       {:ok, socket}
     end
   end
 
+  # Do not log in the user after reset password to avoid a
+  # leaked token giving the user access to the account.
   def handle_event("reset_password", %{"user" => user_params}, socket) do
     case Accounts.reset_user_password(socket.assigns.user, user_params) do
       {:ok, _} ->
         socket
-        |> put_flash(:info, "Password Reset Successfully")
+        |> put_flash(:info, "Password reset successfully.")
         |> redirect(to: Routes.user_login_path(socket, :new))
 
         {:noreply, socket}
 
       {:error, changeset} ->
-        {:noreply,
-         socket
-         |> assign(:changeset, changeset)}
+        {:noreply, assign(socket, :changeset, changeset)}
     end
   end
 
@@ -39,13 +84,13 @@ defmodule PreubaAuthWeb.UserResetPasswordLive.UserResetPassword do
         user,
         &Routes.user_reset_password_url(socket, :edit, &1)
       )
-
-      socket
-      |> put_flash(
-        :info,
-        "If your email is in our system, you will receive instructions to reset your password shortly"
-      )
-      |> redirect(to: "/")
     end
+
+    socket
+    |> put_flash(
+      :info,
+      "If your email is in our system, you will receive instructions to reset your password shortly."
+    )
+    |> redirect(to: "/")
   end
 end
